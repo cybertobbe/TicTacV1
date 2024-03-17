0637d1baa19f2c67cc2d282a529dc274b8915be6
Tobias Sjöström <143131053+cybertobbe@users.noreply.github.com>
2024-03-17 15:14:41 +0100

    Add function for create playername

--- src/main/java/com/example/tictacv1/SetupController.java
+++ src/main/java/com/example/tictacv1/SetupController.java
@@ -1,9 +1,37 @@
 package com.example.tictacv1;
 
+
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
 public class SetupController {
-      public void setupPLayerName(MouseEvent mouseEvent) {
 
+
+      public TextField nameField;
+      public Button setName;
+      public Label player;
+
+
+
+      public void setupPLayerNameOnMousePressed(MouseEvent mouseEvent) {
+
+            setPLayerName();
+//            String name = nameField.getText();
+//            player.setText("Player: " + name);
+
+      }
+
+
+      public void setupPLayerNameOnEnter(ActionEvent actionEvent) {
+            setPLayerName();
+      }
+
+      public void setPLayerName(){
+            String name = nameField.getText();
+            player.setText("Player: " + name);
       }
 }
